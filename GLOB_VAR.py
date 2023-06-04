c0194cb42450d09d2034c8a02ba033e8498f5b21
solvehk <78316293+solveighk@users.noreply.github.com>
2023-06-04 21:02:18 +0200

    global variables

---
+++ GLOB_VAR.py
@@ -0,0 +1,53 @@
+
+from cost_functions import get_bet_capital_cost, get_dt_capital_cost
+DIESEL_USE_SHORT = 0.4
+DIESEL_USE_MIXED = 0.4
+DIESEL_USE_LONG = 0.4
+
+EL_USE_SHORT = 1.7
+EL_USE_MIXED = 1.7
+EL_USE_LONG =  1.7
+
+TOLL_RATE_DT = 1.37 #yearly toll costs
+TOLL_RATE_BET = 0.33
+DISCOUNT_RATE = 0.075 
+NO_SIMULATIONS = 20000 # Number of Monte Carlo simulations
+
+DAY_CHARGE_RATIO_SHORT = 0.14
+DAY_CHARGE_RATIO_MIXED = 0.36
+DAY_CHARGE_RATIO_LONG = 0.49
+YEARLY_DIST_SHORT = 60000
+YEARLY_DIST_MIXED = 80000
+YEARLY_DIST_LONG = 100000
+
+bet_price_2023 = get_bet_capital_cost(2023)
+bet_price_2024 = get_bet_capital_cost(2024)
+bet_price_2025 = get_bet_capital_cost(2025)
+bet_price_2026 = get_bet_capital_cost(2026)
+bet_price_2027 = get_bet_capital_cost(2027)
+bet_price_2028 = get_bet_capital_cost(2028)
+bet_price_2029 = get_bet_capital_cost(2029)
+bet_price_2030 = get_bet_capital_cost(2030)
+bet_price_2031 = get_bet_capital_cost(2031)
+bet_price_2032 = get_bet_capital_cost(2032)
+bet_price_2033 = get_bet_capital_cost(2033)
+bet_price_2034 = get_bet_capital_cost(2034)
+bet_price_2035 = get_bet_capital_cost(2035)
+bet_price_2036 = get_bet_capital_cost(2036)
+bet_price_2037 = get_bet_capital_cost(2037)
+
+dt_price_2023 = get_dt_capital_cost(2023)
+dt_price_2024 = get_dt_capital_cost(2024)
+dt_price_2025 = get_dt_capital_cost(2025)
+dt_price_2026 = get_dt_capital_cost(2026)
+dt_price_2027 = get_dt_capital_cost(2027)
+dt_price_2028 = get_dt_capital_cost(2028)
+dt_price_2029 = get_dt_capital_cost(2029)
+dt_price_2030 = get_dt_capital_cost(2030)
+dt_price_2031 = get_dt_capital_cost(2031)
+dt_price_2032 = get_dt_capital_cost(2032)
+dt_price_2033 = get_dt_capital_cost(2033)
+dt_price_2034 = get_dt_capital_cost(2034)
+dt_price_2035 = get_dt_capital_cost(2035)
+dt_price_2036 = get_dt_capital_cost(2036)
+dt_price_2037 = get_dt_capital_cost(2037)
